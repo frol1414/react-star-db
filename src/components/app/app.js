5e09bf960bfb32ea579bc683d6e2b9e056077cff
frol1414 <frol1414@gmail.com>
2020-04-05 07:12:56 +0300

    add new items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header/header';
 import RandomPlanet from '../random-planet/random-planet';
-import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+import PeoplePage from "../people-page/people-page";
 
 import './app.css';
+import ErrorIndicator from "../error-indicator/error-indicator";
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
-
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
+export default class App extends Component {
+  
+  state = {
+    showRandomPlanet: true,
+    hasError: false,
+  };
+  
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+  
+  
+  
+  componentDidCatch() {
+    this.setState({hasError: true});
+  }
+  
+  render() {
+    
+    if(this.state.hasError) {
+      return <ErrorIndicator/>
+    }
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+    
+    return (
+      <div className="stardb-app">
+        <Header />
+        { planet }
+        
+        <button
+          className="toggle-planet btn btn-warning btn-lg"
+          onClick={this.toggleRandomPlanet}>
+          Toggle Random Planet
+        </button>
+  
+        <PeoplePage />
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./error-indicator.css";
+import icon from "./death-star.png";
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator">
+      <img src={icon} alt="error"/>
+      <span className="boom">BOOM!</span>
+      <span>Something has gone terribly wrong</span>
+    </div>
+  )
+};
+
+export default ErrorIndicator;
