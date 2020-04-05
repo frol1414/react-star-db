ba36868a221dc1e4fcf20ddfbf3dd601329b1dd1
frol1414 <frol1414@gmail.com>
2020-04-05 12:55:45 +0300

    refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,8 +6,13 @@ import PeoplePage from "../people-page/people-page";
 
 import './app.css';
 import ErrorIndicator from "../error-indicator/error-indicator";
+import ItemList from "../item-list/item-list";
+import PersonDetails from "../person-details/person-details";
+import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component {
+  
+  swapiService = new SwapiService();
   
   state = {
     showRandomPlanet: true,
@@ -49,6 +54,29 @@ export default class App extends Component {
         </button>
   
         <PeoplePage />
+  
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}
+            getData={this.swapiService.getAllPlanets}
+            renderItem={(item) => item.name}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
+        </div>
+  
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}
+            getData={this.swapiService.getAllStarships}
+            renderItem={(item) => item.name}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
+        </div>
+        
       </div>
     );
   }
